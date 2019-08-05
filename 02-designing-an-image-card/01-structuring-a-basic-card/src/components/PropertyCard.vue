0676247fbca27a51778981849e636563238b9cc1
Adam Wathan <adam.wathan@gmail.com>
2019-08-05 12:58:10 -0400

    Add example code for lesson 2.1

---
+++ 02-designing-an-image-card/01-structuring-a-basic-card/src/components/PropertyCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-white border rounded-lg overflow-hidden">
+    <img :src="property.imageUrl" :alt="property.imageAlt">
+    <div class="p-6">
+      <h4>{{ property.title }}</h4>
+      <div>
+        {{ property.beds }} beds &bull; {{ property.baths }} baths
+      </div>
+      <div>
+        {{ property.formattedPrice }} / wk
+      </div>
+      <div>
+        {{ property.rating }}/5 stars (based on {{ property.reviewCount }} reviews)
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['property'],
+}
+</script>
