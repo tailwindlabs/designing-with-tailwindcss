0c5ec5ea86d62e0fcb243a18f4d0a5e986b43c78
Adam Wathan <adam.wathan@gmail.com>
2019-08-06 12:45:45 -0400

    Prep for 2.3

---
+++ 02-designing-an-image-card/03-working-with-svg-icons/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="antialiased text-gray-900">
+    <div class="bg-gray-200 min-h-screen p-8 flex items-center justify-center">
+      <PropertyCard :property="property"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import PropertyCard from './components/PropertyCard'
+
+export default {
+  name: 'app',
+  components: {
+    PropertyCard
+  },
+  data: () => ({
+    property: {
+      imageUrl: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+      imageAlt: 'Rear view of modern home with pool',
+      beds: 3,
+      baths: 2,
+      title: "Modern executive home in the heart of historic Los Angeles",
+      priceInCents: 190000,
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4,
+    }
+  })
+}
+</script>
+
+<style src="./assets/tailwind.css"></style>
