00043add14a5de5d9f1e4142195a373e0d15a1ef
Adam Wathan <adam.wathan@gmail.com>
2019-08-07 12:50:49 -0400

    Add example code for 2.4

--- 02-designing-an-image-card/04-designing-a-badge/src/components/PropertyCard.vue
+++ 02-designing-an-image-card/04-designing-a-badge/src/components/PropertyCard.vue
@@ -2,10 +2,13 @@
   <div class="bg-white border rounded-lg overflow-hidden">
     <img :src="property.imageUrl" :alt="property.imageAlt">
     <div class="p-6">
-      <div class="text-gray-600 text-xs uppercase font-semibold tracking-wide">
-        {{ property.beds }} beds &bull; {{ property.baths }} baths
+      <div class="flex items-baseline">
+        <span class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">New</span>
+        <div class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
+          {{ property.beds }} beds &bull; {{ property.baths }} baths
+        </div>
       </div>
-      <h4 class="font-semibold text-lg leading-tight truncate">{{ property.title }}</h4>
+      <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ property.title }}</h4>
       <div class="mt-1">
         {{ property.formattedPrice }}
         <span class="text-gray-600 text-sm"> / wk</span>
