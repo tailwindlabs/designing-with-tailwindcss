49f9d388902719dd03bd61e90850a5ad76a3e4e2
Adam Wathan <adam.wathan@gmail.com>
2019-09-19 12:36:51 -0400

    Add example code for 3.1

--- 03-building-a-responsive-navbar/01-laying-out-the-elements-with-flexbox/src/components/Navbar.vue
+++ 03-building-a-responsive-navbar/01-laying-out-the-elements-with-flexbox/src/components/Navbar.vue
@@ -1,4 +1,16 @@
 <template>
+  <header class="flex items-center justify-between px-4 py-3 bg-gray-900">
+    <div>
+      <img class="h-8" src="/img/logo-inverted.svg" alt="Workcation">
+    </div>
+    <div>
+      <button type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+          <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+        </svg>
+      </button>
+    </div>
+  </header>
 </template>
 
 <script>
