805797ff273aa5c2d7b040d867805bbe3faf99e4
Adam Wathan <adam.wathan@gmail.com>
2019-09-20 07:56:02 -0400

    Switch to nav element

--- 03-building-a-responsive-navbar/03-making-the-navbar-responsive/src/components/Navbar.vue
+++ 03-building-a-responsive-navbar/03-making-the-navbar-responsive/src/components/Navbar.vue
@@ -13,11 +13,11 @@
         </button>
       </div>
     </div>
-    <div :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
+    <nav :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
       <a href="#" class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">List your property</a>
       <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2">Trips</a>
       <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2">Messages</a>
-    </div>
+    </nav>
   </header>
 </template>
 
