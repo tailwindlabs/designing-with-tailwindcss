b6bcbd0a90a2dad9a86e59a639c0e628cd04eda0
Adam Wathan <adam.wathan@gmail.com>
2019-09-26 14:16:22 -0400

    Prep for 4.2

--- 04-building-a-dropdown-menu/02-positioning-the-dropdown/src/components/AccountDropdown.vue
+++ 04-building-a-dropdown-menu/02-positioning-the-dropdown/src/components/AccountDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="relative">
     <button class="block h-10 w-10 overflow-hidden rounded-full border-2 border-gray-600 focus:outline-none focus:border-white">
       <img
         class="h-full w-full object-cover"
@@ -7,7 +7,7 @@
         alt="Your avatar"
         >
     </button>
-    <div class="mt-2 bg-white rounded-lg w-48 py-2 shadow-xl">
+    <div class="absolute right-0 mt-2 bg-white rounded-lg w-48 py-2 shadow-xl">
       <a href="#account" class="block hover:text-white text-gray-800 px-4 py-2 hover:bg-indigo-500">
         Account settings
       </a>
