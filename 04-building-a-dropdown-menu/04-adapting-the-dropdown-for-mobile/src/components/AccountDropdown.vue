cc7ed1f222c37f3c9ece2ef0bc7a410753976777
Adam Wathan <adam.wathan@gmail.com>
2019-09-27 11:30:03 -0400

    Start prep for 4.4

---
+++ 04-building-a-dropdown-menu/04-adapting-the-dropdown-for-mobile/src/components/AccountDropdown.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="relative">
+    <button @click="isOpen = !isOpen" class="relative z-10 block focus:outline-none focus:border-white">
+      <img
+        class="h-8 w-8 overflow-hidden rounded-full border-2 border-gray-600 object-cover"
+        src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
+        alt="Your avatar"
+        >
+    </button>
+    <div v-if="isOpen" class="absolute right-0 mt-2">
+      <button @click="isOpen = false" class="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default" tabindex="-1"></button>
+      <div class="relative z-10 bg-white rounded-lg w-48 py-2 shadow-xl">
+        <a href="#account" class="block hover:text-white text-gray-800 px-4 py-2 hover:bg-indigo-500">
+          Account settings
+        </a>
+        <a href="#support" class="block hover:text-white text-gray-800 mt-0 px-4 py-2 hover:bg-indigo-500">
+          Support
+        </a>
+        <a href="#sign-out" class="block hover:text-white text-gray-800 mt-0 px-4 py-2 hover:bg-indigo-500">
+          Sign out
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  created() {
+    const handleEscape = (e) => {
+      if (e.key === 'Esc' || e.key === 'Escape') {
+        this.isOpen = false
+      }
+    }
+
+    document.addEventListener('keydown', handleEscape)
+
+    this.$once('hook:beforeDestroy', () => {
+      document.removeEventListener('keydown', handleEscape)
+    })
+  }
+}
+</script>
---
+++ 04-building-a-dropdown-menu/04-adapting-the-dropdown-for-mobile/src/components/PropertyCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="relative pb-5/6">
+      <img class="absolute h-full w-full object-cover rounded-lg shadow-md" :src="property.imageUrl" :alt="property.imageAlt">
+    </div>
+    <div class="relative px-4 -mt-16">
+      <div class="bg-white p-6 rounded-lg shadow-lg">
+        <div class="flex items-baseline">
+          <span class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">New</span>
+          <div class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
+            {{ property.beds }} beds &bull; {{ property.baths }} baths
+          </div>
+        </div>
+        <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ property.title }}</h4>
+        <div class="mt-1">
+          {{ property.formattedPrice }}
+          <span class="text-gray-600 text-sm"> / wk</span>
+        </div>
+        <div class="mt-2 flex items-center">
+          <svg v-for="i in 5" :key="i" viewBox="0 0 24 24" :class="i <= property.rating ? 'text-teal-500' : 'text-gray-400'" class="h-4 w-4 fill-current">
+            <path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z" />
+          </svg>
+          <span class="ml-2 text-gray-600 text-sm">{{ property.reviewCount }} reviews</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['property'],
+}
+</script>
