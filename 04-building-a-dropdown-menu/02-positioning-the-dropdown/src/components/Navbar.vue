e1fab43807c2ddcc01038754c94f5f48efa543fe
Adam Wathan <adam.wathan@gmail.com>
2019-09-27 15:47:32 -0400

    Code for 4.2

--- 04-building-a-dropdown-menu/02-positioning-the-dropdown/src/components/Navbar.vue
+++ 04-building-a-dropdown-menu/02-positioning-the-dropdown/src/components/Navbar.vue
@@ -17,12 +17,18 @@
       <a href="#" class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">List your property</a>
       <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2">Trips</a>
       <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2">Messages</a>
+      <AccountDropdown class="ml-6"/>
     </nav>
   </header>
 </template>
 
 <script>
+import AccountDropdown from './AccountDropdown'
+
 export default {
+  components: {
+    AccountDropdown,
+  },
   data() {
     return {
       isOpen: false,
