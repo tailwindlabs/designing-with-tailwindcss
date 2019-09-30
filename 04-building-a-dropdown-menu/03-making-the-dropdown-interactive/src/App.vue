3ba4f5bc1b6ae6d48d043f7701d3618249a029a7
Adam Wathan <adam.wathan@gmail.com>
2019-09-30 13:11:16 -0400

    Prep for 4.3

---
+++ 04-building-a-dropdown-menu/03-making-the-dropdown-interactive/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="antialiased text-gray-900">
+    <div class="bg-gray-200 min-h-screen">
+      <Navbar/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  },
+}
+</script>
+
+<style src="./assets/tailwind.css"></style>
