4e37608bfe7fa989dc7278f84f05ff145def9c20
Adam Wathan <adam.wathan@gmail.com>
2019-09-30 13:34:53 -0400

    Code for 4.3

--- 04-building-a-dropdown-menu/03-making-the-dropdown-interactive/src/components/AccountDropdown.vue
+++ 04-building-a-dropdown-menu/03-making-the-dropdown-interactive/src/components/AccountDropdown.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="relative">
-    <button class="block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
+    <button @click="isOpen = !isOpen" class="relative z-10 block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
       <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80" alt="Your avatar">
     </button>
-    <div class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
+    <button v-if="isOpen" @click="isOpen = false" tabindex="-1" class="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"></button>
+    <div v-if="isOpen" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
       <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Account settings</a>
       <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Support</a>
       <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Sign out</a>
@@ -12,5 +13,24 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  created() {
+    const handleEscape = (e) => {
+      if (e.key === 'Esc' || e.key === 'Escape') {
+        this.isOpen = false
+      }
+    }
+
+    document.addEventListener('keydown', handleEscape)
+
+    this.$once('hook:beforeDestroy', () => {
+      document.removeEventListener('keydown', handleEscape)
+    })
+  }
+}
 </script>
